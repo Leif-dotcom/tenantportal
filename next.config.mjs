81923892b4beb49f4beb736f151e03cd5a36111c
Leif Bönninger <leifbonninger@Air-von-Leif.fritz.box>
2025-02-19 21:58:01 +0100

    Update Vercel and Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,8 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
+  output: 'standalone',
+  distDir: '.next',
 }
 
 mergeConfig(nextConfig, userConfig)
